f220892c307a67178de9e6d7faea924d2ec027e2
Frimpong Michael <frimpstchalla@gmail.com>
2026-05-30 12:25:39 +0100

    Initial commit of initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Frimpong Michael
+# STUDENT ID:  2425403076
+# Email: 2425403076@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Frimpong Michael, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Printing large blocks of my initials with periods and asterisks
+    print("")   # prints an empty line
+    print("...FMK")
+    print("")   # prints an empty line
 
+    print("..." + "FFFFFFFFFFFF" + "........" + "MMMM....MMMM" + "........" + "KKKK....KKKK" + ".....")
+    print("..." + "FFFFFFFFFFFF" + "........" + "MMMM....MMMM" + "........" + "KKKK...KKKK." + ".....")
+    print("..." + "FFFF........" + "........" + "MM.MM..MM.MM" + "........" + "KKKK..KKKK.." + ".....")
+    print("..." + "FFFF........" + "........" + "MM.MM..MM.MM" + "........" + "KKKK.KKKK..." + ".....")
+    print("..." + "FFFFFFFFFFFF" + "........" + "MM..MMMM..MM" + "........" + "KKKKKKKK...." + ".....")
+    print("..." + "FFFFFFFFFFFF" + "........" + "MM..MMMM..MM" + "........" + "KKKKKKKK...." + ".....")
+    print("..." + "FFFF........" + "........" + "MM...MM...MM" + "........" + "KKKK.KKKK..." + ".....")
+    print("..." + "FFFF........" + "........" + "MM...MM...MM" + "........" + "KKKK..KKKK.." + ".....")
+    print("..." + "FFFF........" + "...**..." + "MM...MM...MM" + "...**..." + "KKKK...KKKK." + "...**")
+    print("..." + "FFFF........" + "...**..." + "MM...MM...MM" + "...**..." + "KKKK....KKKK" + "...**")
 
 
 main()
